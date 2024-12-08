32b5b38d16a60bed2c8ea701c46a01ff131b256b
melisa845 <gunmelisa17@gmail.com>
2024-12-08 23:33:59 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,44 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        List<string> ogrenciliste = new List<string>();
+        Console.WriteLine("Bir sayı girin:");
+        int sayi = Convert.ToInt32(Console.ReadLine());
 
-        int ogrencisayi = 0;
-        Console.WriteLine("ögrenci eklemek için 1'e basın hayır ise 2'ye basın lütfen");
+        int toplam = 0;
 
-        while (true)
+        for (int i = 1; i <= sayi; i++)
         {
-            int karar = 0;
-
-            karar = Convert.ToInt32(Console.ReadLine());
-            if (karar == 1)
-            {
-                Console.WriteLine("Geziye katılacak ögrencilerin ismini giriniz");
-                ogrenciliste.Add(Console.ReadLine());
-                ogrencisayi++;
-                Console.WriteLine("Devam etmek için 1' basın");
-            }
-            else if (karar == 2)
-            {
-                Console.WriteLine("Ekleme işlemi bitti. Toplama eklenen kişi sayısı" + ogrencisayi);
-                break;
-            }
-            else {
-                Console.WriteLine("Geçersiz değer girdiniz. Lütfen tekrar deneyin.");
-            }
-
- Console.WriteLine("Araç kaç kişi?");
-        int kapasite=Convert.ToInt32(Console.ReadLine());
-        if (ogrencisayi <= kapasite) {
-            Console.WriteLine("araç sayı yeterli");
-                }
-        else
-        {
-            Console.WriteLine("kapasite yetersiz");
+            toplam += i;
         }
-        Console.ReadLine();
 
-        }
-       
+        Console.WriteLine("sayıların toplamı="+toplam);
+        Console.ReadKey();
     }
 }
