7cad67537f677a122eb2a81653c7b5bed4e08683
melisa845 <gunmelisa17@gmail.com>
2024-12-08 23:19:59 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
-﻿int not1,not2,not3;
+﻿int not1, not2, not3;
 Console.Write("1.Notu Giriniz:");
-not1=Convert.ToInt32(Console.ReadLine());
+not1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("2.Notu Giriniz:");
 not2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("3.Notu Giriniz:");
 not3 = Convert.ToInt32(Console.ReadLine());
 
-int ortalama = (not1 + not2 + not3)/3;
-if (ortalama >= 50)
-{
-    Console.Write("GEÇTİNİZ");
+int ortalama = (not1 + not2 + not3) / 3;
+switch(ortalama){
+    case (>=50): Console.Write("GEÇTİNİZ");
+        break;
+    case (<50):
+        Console.Write("KALDINIZ");
+        break;
+    default:
+        Console.WriteLine("Lütfen notları giriniz");
+        break;
 }
-else
-{
-    Console.Write("KALDINIZ");
-}
+
+
 Console.ReadKey();
